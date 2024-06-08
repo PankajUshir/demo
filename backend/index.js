64b7b6df93dcb8c827e5196570bb9f65c8d097a4
PankajUshir <pankajushir29@gmail.com>
2024-06-08 21:02:47 +0530

    initial project setup

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+dotenv.config()
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server Running at ${process.env.PORT} !!!`)
+})
