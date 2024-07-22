862b057be9138233118c1956684da4b38fa9928c
PankajUshir <pankajushir29@gmail.com>
2024-07-22 22:46:01 +0530

    connect to the database from the app

---
+++ backend/db/config/config.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+const config = {
+    development: {
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_DATABASE,
+        host: process.env.DATABASE_HOST,
+        dialect: process.env.DATABASE_DIALECT,
+    },
+    test: {
+        username: 'root',
+        password: null,
+        database: 'database_test',
+        host: '127.0.0.1',
+        dialect: 'mysql',
+    },
+    production: {
+        username: 'root',
+        password: null,
+        database: 'database_production',
+        host: '127.0.0.1',
+        dialect: 'mysql',
+    },
+}
+
+module.exports = { config }
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
 const express = require('express')
-const logger = require('./src/common/logger')
 require('dotenv').config()
+const logger = require('./src/common/logger')
+const { sequelize } = require('./db/config/sequelize')
 const app = express()
 
 const port = process.env.PORT
 
-app.listen(port, () => {
-    logger.info(`Server Running at ${port} on ${new Date()}`)
-})
+sequelize
+    .authenticate()
+    .then(() => {
+        app.listen(port, () => {
+            logger.info(
+                `Connection has been established successfully on ${new Date()}`
+            )
+            logger.info(`Server Running at ${port} on ${new Date()}`)
+        })
+    })
+    .catch((error) => {
+        logger.error('Unable to connect to the database:', error)
+    })
