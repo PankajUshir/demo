b3690d254e835829c5e2dc7b2e31ec6b774aebc8
PankajUshir <pankajushir29@gmail.com>
2024-08-28 23:37:17 +0530

    node env add

--- backend/db/config/sequelize.js
+++ backend/db/config/sequelize.js
@@ -1,6 +1,16 @@
 const { Sequelize } = require('sequelize')
-const { config } = require('./config')
+const config = require('./config')
 
-const sequelize = new Sequelize(config.development)
+let configuration
+
+if (process.env.NODE_ENV == 'development') {
+    configuration = config.development
+} else if (process.env.NODE_ENV == 'test') {
+    configuration = config.test
+} else if (process.env.NODE_ENV == 'production') {
+    configuration = config.production
+}
+
+const sequelize = new Sequelize(configuration)
 
 module.exports = { sequelize }
